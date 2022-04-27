d0abf72c868ebc8e0cca2039a04a911238c4d5f0
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-04-27 20:02:35 -0300

    Fixed issues in mobile layout

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const showButtons = document.querySelectorAll('.question-button');
 
 function handleClick(event){
     const clickedButton = event.target;
+    const question = clickedButton.parentNode;
+    const arrow = question.lastElementChild;
+    console.dir(arrow);
     const answerPanel = clickedButton.parentNode.nextElementSibling;
-    if(answerPanel.classList.contains('hidden')){
-        answerPanel.classList.remove('hidden');
-    } else if (!answerPanel.classList.contains('hidden')){
+    if ( !answerPanel.classList.contains('hidden') ){
+        
+        question.style.fontWeight = "";
         answerPanel.classList.add('hidden');
-    }
+    } else if( answerPanel.classList.contains('hidden') ){
+        question.style.fontWeight = "bold";
+        answerPanel.classList.remove('hidden');
+    } 
     return
 }
 
