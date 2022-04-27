976a1961ea8ed092e63958349dc094a7db4af1ec
Gonzalo Ruiz <gon.ruiz.dev@gmail.com>
2022-04-27 20:12:34 -0300

    Added arrow up-and-down animation and set the overflow of the accordion (wrapper)

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ function handleClick(event){
     const clickedButton = event.target;
     const question = clickedButton.parentNode;
     const arrow = question.lastElementChild;
-    console.dir(arrow);
     const answerPanel = clickedButton.parentNode.nextElementSibling;
-    if ( !answerPanel.classList.contains('hidden') ){
-        
-        question.style.fontWeight = "";
-        answerPanel.classList.add('hidden');
-    } else if( answerPanel.classList.contains('hidden') ){
+    if ( answerPanel.classList.contains('hidden') ){
         question.style.fontWeight = "bold";
         answerPanel.classList.remove('hidden');
+        console.log(arrow.classList);
+        arrow.classList.add('rotate');
+        console.log(arrow.classList);
+
+    } else if( !answerPanel.classList.contains('hidden') ){
+        question.style.fontWeight = "";
+        answerPanel.classList.add('hidden');
+        console.log(arrow.classList);
+        arrow.classList.remove('rotate');
+        console.log(arrow.classList);
     } 
     return
 }
